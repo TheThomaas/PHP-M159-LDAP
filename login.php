0d9630c28926a4f06b83a48599b3d92b3d0e669e
Thomas Amstutz <54527950+ThomasAmstutz@users.noreply.github.com>
2020-11-15 20:18:06 +0100

    Ajout de la gestion de la connexion

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>M159 - LDAP</title>
+	<title>Connexion - M159 - LDAP</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -22,13 +22,23 @@
 				<input class="signup__input" type="password" name="password" id="password" required />
 				<label class="signup__label" for="password">Mot de passe</label>
 			</div>	
-			<?php 
+			<?php
+
+				// Initialise la session
+				session_start();
+			
+				// Vérifie si l'utilisateur est déjà connecté, si oui il est redirigé
+				if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+					header("location: account.php");
+					exit;
+				}
+
 				if(array_key_exists('createUser', $_POST)){ 
 					$ldapConnection = setupConnection();
 					connectUser($ldapConnection, $_POST['username'], $_POST['password']);
 				} 
 			?>
-			<button class="createUser" type="submit" name="createUser" id="createUser" value="Créer un compte">Se connecter</button>
+			<button class="createUser" type="submit" name="createUser" id="createUser" value="Se connecter">Se connecter</button>
 		</form>
 		<a class="login" href="signup.php">ou créer un compte</a>
 	</div>
